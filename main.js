d21156b9bedb25755e91c1e8406446d7ff359a92
CleveeJ <willsoncj285@gmail.com>
2025-10-18 23:23:50 +0700

    Chore: Move all Bellossom Object from main to bellossom.js

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-import { Ellipsoid } from "./Objects/Ellipsoid.js";
-import { Capsule } from "./Objects/Capsule.js";
-import { Leaf } from "./Objects/Leaf.js";
-import { Toroid } from "./Objects/Toroid.js";
+import { Bellossom } from "./bellosom.js";
 function main() {
     //GET CANVAS
     var CANVAS = document.getElementById("mycanvas");
@@ -120,192 +117,9 @@ function main() {
 
     /*========================= OBJECTS ========================= */
 
-    // ======================== BAGIAN DEFINISI ===================
+    var BellossomObject = new Bellossom(GL, SHADER_PROGRAM, _position, _color);
 
-    //Kepala, Badan, Tangan
-    var Head = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, 1.7, 1.7, 1.7, 20, 20, [0.7686, 0.8588, 0.6118]);
-    var Body = new Capsule(GL, SHADER_PROGRAM, _position, _color, 1.2, 0.5, 1.2, 2, 20, 10, [0.3, 0.1, 0.6118]);
-    var Right_Hand = new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.3, 0.3, 0.3, 2, 20, 10, [0.8, 0.2, 0.6118]);
-    var Left_Hand = new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.3, 0.3, 0.3, 2, 20, 10, [0.5, 0.6, 0.6118]);
-
-    //Rok Luar
-    var Skirt1 = [
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.0, 0.7, 0.1]), -1/6],
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.0, 0.7, 0.1]), 1/6],
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.9569, 0.9059, 0.5333]), 3/6],
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.0, 0.7, 0.1]), 5/6],
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.0, 0.7, 0.1]), 7/6],
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.9569, 0.9059, 0.5333]), 9/6],
-    ];
-
-    //Rok Dalam
-    var Skirt2 = [
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.9569, 0.9059, 0.5333]), 0],
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.0, 0.7, 0.1]), 1/3],
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.0, 0.7, 0.1]), 2/3],
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.9569, 0.9059, 0.5333]), 1],
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.0, 0.7, 0.1]), 4/3],
-        [new Leaf(GL, SHADER_PROGRAM, _position, _color, 1.0, 0.8, 20, 36, [0.0, 0.7, 0.1]), 5/3],
-    ];
-
-    //Bunga di sisi Kepala Kiri
-    var Flower_Torus1 = new Toroid(GL, SHADER_PROGRAM, _position, _color, 0.4, 0.4, 0.2, 0.2, 20, 10, [0.3, 0.1, 0.6118]);
-    var Flower_Elipsoid1 = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, 0.4, 0.4, 0.1, 20, 20, [0.8, 0.2, 0.6118]);
-    var Sepals1 = [
-        [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 1/5],
-        [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 3/5],
-        [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 5/5],
-        [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 7/5],
-        [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 9/5],
-    ]
-
-    //Bunga di sisi Kepala Kanan
-    var Flower_Torus2 = new Toroid(GL, SHADER_PROGRAM, _position, _color, 0.4, 0.4, 0.2, 0.2, 20, 10, [0.3, 0.1, 0.6118]);
-    var Flower_Elipsoid2 = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, 0.4, 0.4, 0.1, 20, 20, [0.8, 0.2, 0.6118]);
-    var Sepals2 = [
-        [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 1/5],
-        [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 3/5],
-        [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 5/5],
-        [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 7/5],
-        [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 9/5],
-    ]
-
-    // Mata
-    var LeftEye = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, 0.2, 0.2, 0.2, 20, 20, [0, 0, 0]);
-    var RightEye = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, 0.2, 0.2, 0.2, 20, 20, [0, 0, 0]);
-
-    // ============= END OF DEFINISI ===================
-
-    // ============= BAGIAN HIERARKI NYA ===============
-
-    Body.childs.push(Head);
-    Body.childs.push(Right_Hand);
-    Body.childs.push(Left_Hand);
-    Head.childs.push(Flower_Torus1);
-    Head.childs.push(LeftEye);
-    Head.childs.push(RightEye);
-    Flower_Torus1.childs.push(Flower_Elipsoid1);
-    Head.childs.push(Flower_Torus2);
-    Flower_Torus2.childs.push(Flower_Elipsoid2);
-
-    for (let index = 0; index < Sepals1.length; index++) {
-        const element = Sepals1[index];
-        Flower_Torus1.childs.push(element[0]);
-    }
-
-    for (let index = 0; index < Sepals2.length; index++) {
-        const element = Sepals2[index];
-        Flower_Torus2.childs.push(element[0]);
-    }
-
-    for (let index = 0; index < Skirt1.length; index++) {
-        const element = Skirt1[index];
-        Body.childs.push(element[0]);
-    }
-
-    for (let index = 0; index < Skirt1.length; index++) {
-        const element = Skirt2[index];
-        Body.childs.push(element[0]);
-    }
-
-    // ==================== END OF HIERARKI =======================
-
-    // ==================== BAGIAN POSITIONING NYA ==================
-    //Bunga sisi kiri kepala
-    LIBS.translateZ(Flower_Torus1.POSITION_MATRIX, 1.7);
-    var temp = LIBS.get_I4();
-    LIBS.rotateY(temp, Math.PI/3);
-    LIBS.rotateZ(temp,Math.PI/4.5);
-    Flower_Torus1.POSITION_MATRIX = LIBS.multiply(Flower_Torus1.POSITION_MATRIX, temp);
-    LIBS.translateZ(Flower_Elipsoid1.POSITION_MATRIX, -0.05);
-    
-    for (let index = 0; index < Sepals1.length; index++) {
-        const element = Sepals1[index];
-        
-        LIBS.translateY(element[0].POSITION_MATRIX, -0.8);
-        var temp = LIBS.get_I4();
-        LIBS.rotateZ(temp, Math.PI * element[1]);
-        element[0].POSITION_MATRIX = LIBS.multiply(element[0].POSITION_MATRIX, temp);
-        
-        LIBS.translateZ(element[0].POSITION_MATRIX, -0.1)
-    }
-
-    //Bunga sisi Kanan kepala
-    LIBS.translateZ(Flower_Torus2.POSITION_MATRIX, 1.7);
-    var temp = LIBS.get_I4();
-    LIBS.rotateY(temp, -Math.PI/3);
-    LIBS.rotateZ(temp, -Math.PI/4.5);
-    Flower_Torus2.POSITION_MATRIX = LIBS.multiply(Flower_Torus2.POSITION_MATRIX, temp);
-    LIBS.translateZ(Flower_Elipsoid2.POSITION_MATRIX, -0.05);
-    
-    for (let index = 0; index < Sepals2.length; index++) {
-        const element = Sepals2[index];
-
-        LIBS.translateY(element[0].POSITION_MATRIX, -0.8);
-        var temp = LIBS.get_I4();
-        LIBS.rotateZ(temp, Math.PI * element[1]);
-        element[0].POSITION_MATRIX = LIBS.multiply(element[0].POSITION_MATRIX, temp);
-
-        LIBS.translateZ(element[0].POSITION_MATRIX, -0.1)
-    }
-
-    //Kepala
-    LIBS.translateY(Head.POSITION_MATRIX, 2);
-
-    //Mata Kiri
-    LIBS.translateZ(LeftEye.POSITION_MATRIX, 1.6);
-    LIBS.translateY(LeftEye.POSITION_MATRIX, 0.2);
-    LIBS.translateX(LeftEye.POSITION_MATRIX, 0.5);
-
-    //Mata Kanan
-    LIBS.translateZ(RightEye.POSITION_MATRIX, 1.6);
-    LIBS.translateY(RightEye.POSITION_MATRIX, 0.2);
-    LIBS.translateX(RightEye.POSITION_MATRIX, -0.5);
-
-    //Tangan Kanan
-    LIBS.rotateX(Right_Hand.POSITION_MATRIX, Math.PI / 2);
-    LIBS.rotateY(Right_Hand.POSITION_MATRIX, -Math.PI / 3);
-    LIBS.translateX(Right_Hand.POSITION_MATRIX, -1);
-    LIBS.translateZ(Right_Hand.POSITION_MATRIX, 0.7);
-    LIBS.translateY(Right_Hand.POSITION_MATRIX, 0.5);
-
-    //Tangan Kiri
-    LIBS.rotateX(Left_Hand.POSITION_MATRIX, Math.PI / 2);
-    LIBS.rotateY(Left_Hand.POSITION_MATRIX, Math.PI / 3);
-    LIBS.translateX(Left_Hand.POSITION_MATRIX, 1);
-    LIBS.translateZ(Left_Hand.POSITION_MATRIX, 0.7);
-    LIBS.translateY(Left_Hand.POSITION_MATRIX, 0.5);
-
-    //Rok Luar
-    for (let index = 0; index < Skirt1.length; index++) {
-        const element = Skirt1[index];
-
-        LIBS.translateZ(element[0].POSITION_MATRIX, -1.7);
-        LIBS.rotateX(element[0].POSITION_MATRIX, Math.PI/6)
-        LIBS.scaleX(element[0].POSITION_MATRIX, 1.3);
-        var temp = LIBS.get_I4();
-        LIBS.rotateY(temp, Math.PI * element[1]);
-        element[0].POSITION_MATRIX = LIBS.multiply(element[0].POSITION_MATRIX, temp);
-
-        LIBS.translateY(element[0].POSITION_MATRIX, -0.5)
-    }
-
-    //Rok Dalam
-    for (let index = 0; index < Skirt2.length; index++) {
-        const element = Skirt2[index];
-
-        LIBS.translateZ(element[0].POSITION_MATRIX, -0.8);
-        LIBS.rotateX(element[0].POSITION_MATRIX, Math.PI/6)
-        var temp = LIBS.get_I4();
-        LIBS.rotateY(temp, Math.PI * element[1]);
-        element[0].POSITION_MATRIX = LIBS.multiply(element[0].POSITION_MATRIX, temp);
-
-        LIBS.translateY(element[0].POSITION_MATRIX, -0.5)
-    }
-
-    // ======================= END OF POSITIONING =======================
-
-    Body.setup();
+    BellossomObject.setup();
     
     // ------------------------- END -----------------------
 
@@ -349,7 +163,7 @@ function main() {
 
         GL.uniformMatrix4fv(_Vmatrix, false, VIEWMATRIX);
         
-        Body.render(_Mmatrix, LIBS.get_I4());
+        BellossomObject.render(_Mmatrix, LIBS.get_I4());
 
         GL.flush();
         window.requestAnimationFrame(animate);
