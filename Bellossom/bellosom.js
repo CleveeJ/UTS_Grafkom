49a8b4bffa324fdba66d628015097e77d074dc0a
CleveeJ <willsoncj285@gmail.com>
2025-10-20 21:17:09 +0700

    Revert "Chore: change color"
    This reverts commit cc728b4d27caf05af5d1b76881b3d0f50bf7ec93.

--- Bellossom/bellosom.js
+++ Bellossom/bellosom.js
@@ -9,10 +9,10 @@ export class Bellossom{
         // ======================== BAGIAN DEFINISI ===================
 
         //Kepala, Badan, Tangan
-        var Head = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, _normal, 1.7, 1.7, 1.7, 20, 20, [0.7686, 0.8588, 0.6118]);
-        var Body = new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 1.2, 0.5, 1.2, 2, 20, 10, [0.7686, 0.8588, 0.6118]);
-        var Right_Hand = new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.3, 0.3, 0.3, 2, 20, 10, [0.7686, 0.8588, 0.6118]);
-        var Left_Hand = new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.3, 0.3, 0.3, 2, 20, 10, [0.7686, 0.8588, 0.6118]);
+        var Head = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, 1.7, 1.7, 1.7, 20, 20, [0.7686, 0.8588, 0.6118]);
+        var Body = new Capsule(GL, SHADER_PROGRAM, _position, _color, 1.2, 0.5, 1.2, 2, 20, 10, [0.3, 0.1, 0.6118]);
+        var Right_Hand = new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.3, 0.3, 0.3, 2, 20, 10, [0.8, 0.2, 0.6118]);
+        var Left_Hand = new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.3, 0.3, 0.3, 2, 20, 10, [0.5, 0.6, 0.6118]);
 
         //Rok Luar
         var Skirt1 = [
@@ -35,25 +35,25 @@ export class Bellossom{
         ];
 
         //Bunga di sisi Kepala Kiri
-        var Flower_Torus1 = new Toroid(GL, SHADER_PROGRAM, _position, _color, _normal, 0.4, 0.4, 0.2, 0.2, 20, 10, [0.9921568627450981, 0.8980392156862745, 0.5058823529411764]);
-        var Flower_Elipsoid1 = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, _normal, 0.4, 0.4, 0.1, 20, 20, [0.9921568627450981, 0.8980392156862745, 0.5058823529411764]);
+        var Flower_Torus1 = new Toroid(GL, SHADER_PROGRAM, _position, _color, 0.4, 0.4, 0.2, 0.2, 20, 10, [0.3, 0.1, 0.6118]);
+        var Flower_Elipsoid1 = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, 0.4, 0.4, 0.1, 20, 20, [0.8, 0.2, 0.6118]);
         var Sepals1 = [
-            [new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.55, 0.3, 0.1, 1, 20, 10, [0.9294117647058824, 0.4549019607843137, 0.2784313725490196]), 1/5],
-            [new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.55, 0.3, 0.1, 1, 20, 10, [0.9294117647058824, 0.4549019607843137, 0.2784313725490196]), 3/5],
-            [new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.55, 0.3, 0.1, 1, 20, 10, [0.9294117647058824, 0.4549019607843137, 0.2784313725490196]), 5/5],
-            [new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.55, 0.3, 0.1, 1, 20, 10, [0.9294117647058824, 0.4549019607843137, 0.2784313725490196]), 7/5],
-            [new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.55, 0.3, 0.1, 1, 20, 10, [0.9294117647058824, 0.4549019607843137, 0.2784313725490196]), 9/5],
+            [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 1/5],
+            [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 3/5],
+            [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 5/5],
+            [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 7/5],
+            [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 9/5],
         ]
 
         //Bunga di sisi Kepala Kanan
-        var Flower_Torus2 = new Toroid(GL, SHADER_PROGRAM, _position, _color, _normal, 0.4, 0.4, 0.2, 0.2, 20, 10, [0.9921568627450981, 0.8980392156862745, 0.5058823529411764]);
-        var Flower_Elipsoid2 = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, _normal, 0.4, 0.4, 0.1, 20, 20, [0.9921568627450981, 0.8980392156862745, 0.5058823529411764]);
+        var Flower_Torus2 = new Toroid(GL, SHADER_PROGRAM, _position, _color, 0.4, 0.4, 0.2, 0.2, 20, 10, [0.3, 0.1, 0.6118]);
+        var Flower_Elipsoid2 = new Ellipsoid(GL, SHADER_PROGRAM, _position, _color, 0.4, 0.4, 0.1, 20, 20, [0.8, 0.2, 0.6118]);
         var Sepals2 = [
-            [new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.55, 0.3, 0.1, 1, 20, 10, [0.9294117647058824, 0.4549019607843137, 0.2784313725490196]), 1/5],
-            [new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.55, 0.3, 0.1, 1, 20, 10, [0.9294117647058824, 0.4549019607843137, 0.2784313725490196]), 3/5],
-            [new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.55, 0.3, 0.1, 1, 20, 10, [0.9294117647058824, 0.4549019607843137, 0.2784313725490196]), 5/5],
-            [new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.55, 0.3, 0.1, 1, 20, 10, [0.9294117647058824, 0.4549019607843137, 0.2784313725490196]), 7/5],
-            [new Capsule(GL, SHADER_PROGRAM, _position, _color, _normal, 0.55, 0.3, 0.1, 1, 20, 10, [0.9294117647058824, 0.4549019607843137, 0.2784313725490196]), 9/5],
+            [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 1/5],
+            [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 3/5],
+            [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 5/5],
+            [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 7/5],
+            [new Capsule(GL, SHADER_PROGRAM, _position, _color, 0.55, 0.3, 0.1, 1, 20, 10, [0.3, 0.5, 0.6]), 9/5],
         ]
 
         // Mata
@@ -139,12 +139,12 @@ export class Bellossom{
         LIBS.translateY(Head.POSITION_MATRIX, 2);
 
         //Mata Kiri
-        LIBS.translateZ(LeftEye.POSITION_MATRIX, 1.5);
+        LIBS.translateZ(LeftEye.POSITION_MATRIX, 1.6);
         LIBS.translateY(LeftEye.POSITION_MATRIX, 0.2);
         LIBS.translateX(LeftEye.POSITION_MATRIX, 0.5);
 
         //Mata Kanan
-        LIBS.translateZ(RightEye.POSITION_MATRIX, 1.5);
+        LIBS.translateZ(RightEye.POSITION_MATRIX, 1.6);
         LIBS.translateY(RightEye.POSITION_MATRIX, 0.2);
         LIBS.translateX(RightEye.POSITION_MATRIX, -0.5);
 
